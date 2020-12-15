2296013920a90a86e06c2304287f1d889be10f98
ACCount-Nine38 <acc938live@gmail.com>
2020-12-15 13:02:19 -0500

    New Chickens and Render Order
    adding in new chickens and render order

--- Chicken Farm/Assets/Player.cs
+++ Chicken Farm/Assets/Player.cs
@@ -15,6 +15,10 @@ public class Player : Photon.MonoBehaviour
     public float MoveSpeed;
     private Vector2 moveDirection;
 
+    // spawnable objects for testing
+    public GameObject normalChicken;
+    public GameObject thinChicken;
+
     // Awake() is called when photon network is initiated
     private void Awake()
     {
@@ -23,11 +27,12 @@ public class Player : Photon.MonoBehaviour
         {
             PlayerCamera.SetActive(true);
             PlayerNameText.text = PhotonNetwork.playerName;
+            PlayerNameText.color = Color.yellow;
         }
         else
         {
             PlayerNameText.text = photonView.owner.name;
-            PlayerNameText.color = Color.yellow;
+            PlayerNameText.color = Color.white;
         }
     }
 
@@ -55,25 +60,33 @@ public class Player : Photon.MonoBehaviour
         moveDirection = new Vector2(moveX, moveY).normalized;
 
         // keyboard controls
-        if (Input.GetKeyDown(KeyCode.A))
+        if (rb.velocity.x < 0)
         {
             photonView.RPC("FlipFalse", PhotonTargets.AllBuffered);
         }
 
-        if (Input.GetKeyDown(KeyCode.D))
+        else if (rb.velocity.x > 0)
         {
             photonView.RPC("FlipTrue", PhotonTargets.AllBuffered);
         }
 
         // animation updates
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
         {
-            anim.SetBool("isRunning", true);
+            anim.SetBool("isMoving", true);
         }
-
         else
         {
-            anim.SetBool("isRunning", false);
+            anim.SetBool("isMoving", false);
+        }
+
+        // spawns a chicken for testing
+        if(Input.GetKeyDown(KeyCode.Alpha1)) {
+            PhotonNetwork.Instantiate(normalChicken.name, new Vector2(this.transform.position.x, this.transform.position.y), Quaternion.identity, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            PhotonNetwork.Instantiate(thinChicken.name, new Vector2(this.transform.position.x, this.transform.position.y), Quaternion.identity, 0);
         }
     }
 
---
+++ Chicken Farm/Assets/ThinChicken.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThinChicken : Chicken
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+}
